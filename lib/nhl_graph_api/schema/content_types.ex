96f92a3eb1877cf4f6d3247bf5a03e6f7fa19304
Scott Moak <scott.moak@mybrainoncode.com>
2018-01-25 21:13:50 -0800

    Add dataloader
    Also fix the plug pipeline so we stop getting "The response was already
    sent" errors.
    
    Using fixture data for now.

--- lib/nhl_graph_api/schema/content_types.ex
+++ lib/nhl_graph_api/schema/content_types.ex
@@ -2,9 +2,9 @@ defmodule NhlGraphApi.Schema.ContentTypes do
   use Absinthe.Schema.Notation
 
   object :team do
-    field(:id, :id)
-    field(:name, :string)
-    field(:city, :string)
-    field(:abbreviation, :string)
+    field(:id, non_null(:id))
+    field(:name, non_null(:string))
+    field(:city, non_null(:string))
+    field(:abbreviation, non_null(:string))
   end
 end
