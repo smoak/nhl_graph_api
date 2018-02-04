4439ca29b25acd2fa589a74a3918bcb77f44405b
Sasha Moak <sasha.moak@gmail.com>
2018-02-03 22:48:48 -0800

    Make a NhlStatsApi module that can fetch teams
    Use said module to do the thing

---
+++ lib/nhl_graph_api/clients/nhl_stats_api.ex
@@ -0,0 +1,21 @@
+defmodule NhlGraphApi.Clients.NhlStatsApi do
+  use HTTPotion.Base
+
+  def process_url(url) do
+    "http://statsapi.web.nhl.com/api/v1/" <> url
+  end
+
+  def process_response_body(body) do
+    body
+    |> IO.iodata_to_binary()
+    |> Poison.decode!()
+  end
+
+  def teams() do
+    get("/teams").body["teams"]
+  end
+
+  def team(id) do
+    get("/teams/#{id}").body["teams"] |> List.first()
+  end
+end
