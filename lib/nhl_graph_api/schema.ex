e47bbf83d9fb55eaaeebb2307cf72a640a9d1f7b
Scott Moak <scott.moak@mybrainoncode.com>
2018-02-03 21:42:13 -0800

    Remove the special load method and just use the dataloader helper.
    Fix up the fetch/2 method so that it returns correct results.

--- lib/nhl_graph_api/schema.ex
+++ lib/nhl_graph_api/schema.ex
@@ -19,28 +19,11 @@ defmodule NhlGraphApi.Schema do
   end
 
   query do
-    field(:teams, list_of(:team), resolve: load(Nhl))
+    field(:teams, list_of(:team), resolve: dataloader(Nhl))
 
     field :team, :team do
       arg(:id, non_null(:id))
-      resolve(load(Nhl))
-    end
-  end
-
-  # Reusable dataloader helper replacement for simple KV use.
-  #
-  # This serves the same purpose as the `dataloader` helper, but
-  # as that's currently focused on batching by parent (object) for Ecto,
-  # we (currently) need to do some special handling.
-  def load(source_name) do
-    fn _, args, %{context: %{loader: loader}} ->
-      # The `nil` values here is the batch key; all batching
-      # for the source automatically done together.
-      loader
-      |> Dataloader.load(source_name, nil, args)
-      |> on_load(fn loader ->
-        {:ok, Dataloader.get(loader, source_name, nil, args)}
-      end)
+      resolve(dataloader(Nhl))
     end
   end
 end
