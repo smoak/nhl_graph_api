d9439fbee214ae0d34a08aebe646bf57ee39dda7
Scott Moak <scott.moak@mybrainoncode.com>
2018-02-16 00:23:28 -0800

    Using a more sane Dataloader loading mechanism
    The dataloader/1 helper seems to put the id in the batch:
    
    [batch: {:team, %{id: "5"}}, ids: [%{}]]
    
    This makes the loader behavior a bit odd because the result was always a
    map with a key of %{} (because thats what the args were). Instead a
    custom load function is used so that the batch is just the resource and
    the args are passed in properly (as a map of args for the field):
    
    [batch: :team, args: [%{id: "5"}]]
    
    This makes the fetch/2 method a little more clearer. It now needs to
    return a map with keys from the args.

--- lib/nhl_graph_api/loaders/nhl.ex
+++ lib/nhl_graph_api/loaders/nhl.ex
@@ -1,39 +1,35 @@
 defmodule NhlGraphApi.Loaders.Nhl do
-  @teams %{
-    "1" => %{
-      id: 1,
-      name: "foo team",
-      city: "foo city",
-      abbreviation: "foo abbrev"
-    }
-  }
-
   alias NhlGraphApi.Clients.NhlStatsApi
 
   def data() do
     Dataloader.KV.new(&fetch/2)
   end
 
-  def fetch({:teams, _}, _) do
-    teams =
-      NhlStatsApi.teams()
-      |> Enum.map(fn json -> team_from_json(json) end)
-
-    %{%{} => teams}
+  def fetch(:team, %{id: id}) do
+    %{ %{id: id} => fetch_team_by_id(id) }
   end
 
-  def fetch({:team, %{id: id}}, _) do
-    # must return a map keyed by args which is always %{}
-    %{%{} => fetch_team_by_id(id)}
+  def fetch(:team, args) do
+    args
+    |> Enum.reduce(%{}, fn (arg, result) -> result |> Map.merge(fetch(:team, arg)) end)
   end
 
-  def fetch(batch, ids) do
-    IO.inspect(batch: batch, ids: ids)
+  def fetch(:teams, _) do
+    %{%{} => fetch_all_teams()}
+  end
+
+  def fetch(batch, args) do
+    IO.inspect(batch: batch, args: args)
     %{}
   end
 
   def fetch_team_by_id(id) do
     NhlStatsApi.team(id) |> team_from_json
+  end
+
+  def fetch_all_teams() do
+    NhlStatsApi.teams()
+    |> Enum.map(&team_from_json/1)
   end
 
   defp team_from_json(json) do
